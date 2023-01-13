1e4085d7d743bce32235511e6e2bf41ff4a60dbf
SafonovaEkaterina <doinikova-ekaterina@yandex.ru>
2023-01-13 16:46:01 +0300

    Refresh

--- homework.py
+++ homework.py
@@ -1,16 +1,34 @@
 import logging
+import os
 import sys
 import time
 from http import HTTPStatus
 
 import requests
 import telegram
+from dotenv import load_dotenv
 
-from config import (ENDPOINT, ENVLIST, HEADERS, HOMEWORK_VERDICTS,
-                    PRACTICUM_TOKEN, RETRY_PERIOD, TELEGRAM_CHAT_ID,
-                    TELEGRAM_TOKEN)
 from exceptions import (APIResponseError, CurrentDateError,
                         MassageNotSentError, RequestAPIError)
+
+load_dotenv()
+
+
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+
+RETRY_PERIOD = 600
+ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
+HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+
+ENVLIST = ['PRACTICUM_TOKEN', 'TELEGRAM_TOKEN', 'TELEGRAM_CHAT_ID']
+
+HOMEWORK_VERDICTS = {
+    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
+    'reviewing': 'Работа взята на проверку ревьюером.',
+    'rejected': 'Работа проверена: у ревьюера есть замечания.'
+}
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -47,7 +65,7 @@ def send_message(bot, message):
 
 def get_api_answer(timestamp):
     """Получение ответа API."""
-    logging.info('Запрос к API отправляется')
+    logging.info('Запрос к API c отправляется')
     timestamp = timestamp or int(time.time())
     params = {'from_date': timestamp}
 
